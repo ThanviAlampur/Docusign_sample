7da89b772797bef4756e4d59ab387bc2b6c7fb04
Alampurthanvi <talampur@pharmateksol.com>
2024-12-04 15:53:59 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,7 +20,7 @@ from py.xml import html
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="edge")
+    parser.addoption("--browser", action="store", default="chrome")
 
 
 @pytest.fixture(scope='class')
